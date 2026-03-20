e8fbe98a20bc03b561e52bf2b2f42027b6436d13
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2026-03-20 14:16:59 +0100

    test: Unit and integration tests of reading PDF files

--- tests/integration/tests_app/tests_utils/test_integration_pdf_loader.py
+++ tests/integration/tests_app/tests_utils/test_integration_pdf_loader.py
@@ -1,36 +1,36 @@
+from pathlib import Path
+
 import pytest
 
 from app.utils.pdf_loader import MedicalPDFLoader
 
 class TestPdfLoader:
 
+    @pytest.fixture(scope="class", autouse=True)
+    def setup_base_path(self, request):
+        # Asignamos el atributo a la clase para que las subclases lo vean
+        request.cls.base_path = Path(__file__).parent.parent.parent
+
     class TestInit:
 
-        def test_init_not_exists(self, fs):
-            file_path = "/home/mjesus/Escritorio/Medical Interaction Assistant/tests/data/Example.pdf"
+        def test_init_not_exists(self):
+            file_path = str(self.base_path / "data" / "Example.pdf")
 
             with pytest.raises(FileNotFoundError) as file_error:
                 MedicalPDFLoader(file_path)
 
                 assert "doesn't exist" in str(file_error.value)
 
+        def test_init_not_file(self):            
+            file_path = str(self.base_path / "data")
 
-        def test_init_not_file(self, fs):
-            
-            file_path = "/home/mjesus/Escritorio/Medical Interaction Assistant/tests/data"
-            fs.create_file(file_path)
-
-
-            with pytest.raises(ValueError) as value_error:
+            with pytest.raises(FileNotFoundError) as file_error:
                 MedicalPDFLoader(file_path)
 
-                assert "isn't a PDF file" in str(value_error.value)
+                assert "doesn't exist" in str(file_error.value)
 
-        def test_init_not_pdf(self, fs):
-            
-            file_path = "/home/mjesus/Escritorio/Medical Interaction Assistant/tests/data/example.txt"
-            fs.create_file(file_path)
-
+        def test_init_not_pdf(self):
+            file_path = str(self.base_path / "data" / "example.txt")
 
             with pytest.raises(ValueError) as value_error:
                 MedicalPDFLoader(file_path)
@@ -38,8 +38,29 @@ class TestPdfLoader:
                 assert "isn't a PDF file" in str(value_error.value)
 
         def test_init_pdf_file(self):
-            file_path = "/home/mjesus/Escritorio/Medical Interaction Assistant/tests/data/example.pdf"
+            file_path = str(self.base_path / "data" / "example.pdf")
 
             pdfLoader = MedicalPDFLoader(file_path)
             assert hasattr(pdfLoader, "file_path")
             assert pdfLoader.file_path == file_path
+
+    class TestReadPdf:
+
+        def test_no_read_pdf(self, caplog):
+            pdf_path = str(self.base_path / "data" / "corrupt_example.pdf")
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+            paragraphs = pdf_loader._read_pdf()
+
+            assert paragraphs == []
+            assert "Error processing" in caplog.text
+
+
+        def test_read_pdf(self):
+            pdf_path = str(self.base_path / "data" / "example.pdf")
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+            paragraphs = pdf_loader._read_pdf()
+
+            assert len(paragraphs) == 1
+            assert "This is a PDF example" in paragraphs
--- tests/unit/tests_app/tests_utils/test_unit_pdf_loader.py
+++ tests/unit/tests_app/tests_utils/test_unit_pdf_loader.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+import fitz
 import pytest
 
 from app.utils.pdf_loader import MedicalPDFLoader
@@ -18,13 +21,12 @@ class TestPdfLoader:
         def test_init_not_file(self, fs):
             
             file_path = "/data/inputs_pdfs/"
-            fs.create_file(file_path)
+            fs.create_dir(file_path)
 
-
-            with pytest.raises(ValueError) as value_error:
+            with pytest.raises(FileNotFoundError) as file_error:
                 MedicalPDFLoader(file_path)
 
-                assert "isn't a PDF file" in str(value_error.value)
+                assert "doesn't exist" in str(file_error.value)
 
         def test_init_not_pdf(self, fs):
             
@@ -42,6 +44,46 @@ class TestPdfLoader:
             file_path = "/data/inputs_pdfs/Ejemplo.pdf"
             fs.create_file(file_path)
 
-            pdfLoader = MedicalPDFLoader(file_path)
-            assert hasattr(pdfLoader, "file_path")
-            assert pdfLoader.file_path == file_path
+            pdf_loader = MedicalPDFLoader(file_path)
+            assert hasattr(pdf_loader, "file_path")
+            assert pdf_loader.file_path == file_path
+
+    class TestReadPdf:
+
+        def _create_fake_pdf(self, pdf_path, fs):  
+            # Creation of PDF file
+            doc_temp = fitz.open()
+            page = doc_temp.new_page()
+            page.insert_text((50, 50), "This is a PDF example")
+            pdf_bytes = doc_temp.tobytes()
+            doc_temp.save(pdf_path)
+            doc_temp.close()
+
+            # Creation of PDF path
+            fs.create_file(pdf_path, contents=pdf_bytes)
+
+        def _create_corrupt_pdf(self, pdf_path, fs):
+            fs.create_file(pdf_path, contents="Corrupt PDF!")
+
+        def test_no_read_pdf(self, fs, caplog):
+            pdf_path = "data/input_pdfs/corrupt.pdf"
+            self._create_corrupt_pdf(pdf_path, fs)
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+            paragraphs = pdf_loader._read_pdf()
+
+            assert paragraphs == []
+            assert "Error processing" in caplog.text
+
+
+        def test_read_pdf(self, fs):
+            pdf_path = "data/input_pdfs/example.pdf"
+
+            self._create_fake_pdf(pdf_path, fs)
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+            paragraphs = pdf_loader._read_pdf()
+
+            assert len(paragraphs) == 1
+            assert "This is a PDF example" in paragraphs          
+            
