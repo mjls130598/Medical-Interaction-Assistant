b04bd3c013f1f8f16d9545dd5b2f9204fcb13c5f
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2026-03-20 11:11:22 +0100

    test: Integration tests for MedicarPDFLoader constructor

---
+++ tests/integration/tests_app/tests_utils/test_pdf_loader.py
@@ -0,0 +1,43 @@
+import pytest
+
+from app.utils.pdf_loader import MedicalPDFLoader
+
+class TestPdfLoader:
+
+    def test_init_not_exists(self, fs):
+        file_path = "/home/mjesus/Escritorio/Medical Interaction Assistant/tests/data/Example.pdf"
+
+        with pytest.raises(FileNotFoundError) as file_error:
+            MedicalPDFLoader(file_path)
+
+            assert "doesn't exist" in str(file_error.value)
+
+
+    def test_init_not_file(self, fs):
+        
+        file_path = "/home/mjesus/Escritorio/Medical Interaction Assistant/tests/data"
+        fs.create_file(file_path)
+
+
+        with pytest.raises(ValueError) as value_error:
+            MedicalPDFLoader(file_path)
+
+            assert "isn't a PDF file" in str(value_error.value)
+
+    def test_init_not_pdf(self, fs):
+        
+        file_path = "/home/mjesus/Escritorio/Medical Interaction Assistant/tests/data/example.txt"
+        fs.create_file(file_path)
+
+
+        with pytest.raises(ValueError) as value_error:
+            MedicalPDFLoader(file_path)
+
+            assert "isn't a PDF file" in str(value_error.value)
+
+    def test_init_pdf_file(self):
+        file_path = "/home/mjesus/Escritorio/Medical Interaction Assistant/tests/data/example.pdf"
+
+        pdfLoader = MedicalPDFLoader(file_path)
+        assert hasattr(pdfLoader, "file_path")
+        assert pdfLoader.file_path == file_path
