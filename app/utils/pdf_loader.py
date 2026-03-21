5410adb7b2d4f291b080a0caa5dad4947e41a02c
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2026-03-21 12:56:49 +0100

    Feat: Clean PDF text and unify incomplete lines

--- app/utils/pdf_loader.py
+++ app/utils/pdf_loader.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import re
+from typing import List
 import fitz
 import logging
 
@@ -15,26 +17,78 @@ class MedicalPDFLoader:
 
         self.file_path = file_path  
 
+    def _clean_block(self, block:str) -> str:
 
-    def _read_pdf(self):
+        logging.info("Cleaning block")
+
+        # 1. Remove new lines inside words
+        # For example: "ace- \n tilcisteína" -> "acetilcisteína"
+        complete_words = re.sub(r'-\s*\n\s*', '', block)
+
+        # 2. Join lines which doesn't start with -, number or capital letter 
+        pattern = r'\n(?!\s*(?:[-•]|(?:\d+[.)\s])|[A-ZÁÉÍÓÚ]))'
+        complete_sentences = re.sub(pattern, ' ', complete_words)
+        
+        # 3. Concatenate multiple spaces in only one
+        return re.sub(r' +', " ", complete_sentences).strip()
+    
+    def _create_paragraphs(self, blocks) -> List[str]:
+        paragraphs = []
+
+        for idx, block in enumerate(blocks):
+            
+            logging.info(f"Extracting block nº {idx + 1}")
+
+            # If it's not a text
+            if block[6] != 0:
+                logging.info("Not text")
+                continue
+
+            page_pattern = r'\b\d+\s+de\s+\d+\b'
+
+            if re.search(page_pattern, block[4]):
+                logging.info("Removing page number")
+                continue
+
+            text = self._clean_block(block[4])
+            
+            # If there isn't text
+            if not text:
+                logging.info("There isn't text")
+                continue
+            
+            if paragraphs:
+                logging.info("Checking if the last paragraph is not complete and the current text is from the last paragraph")
+                last_paragraph = paragraphs[-1]
+                is_incomplete = not last_paragraph.endswith(('.', ':', '?', '!'))
+                starts_with_low = text[0].islower()
+
+                if is_incomplete or starts_with_low:
+                    logging.info("Concatenate last paragraph with current text")
+                    paragraphs[-1] = f'{last_paragraph} {text}'
+                    continue
+
+            logging.info("Creating new paragraph")                        
+            paragraphs.append(text)
+
+        return paragraphs
+
+
+    def _read_pdf(self) -> List[str]:
 
         try:
             with fitz.open(self.file_path) as doc:
                 
                 logging.info(f"Reading {self.file_path}")
 
-                logging.info("Extracting paragraphs from document")
+                logging.info("Extracting blocks from document")
                 all_blocks = (
                     block for page in doc
                     for block in page.get_text("blocks") # read paragraphs instead of lines
                 )
                 
-                logging.info("Extracting text from paragraphs")
-                paragraphs = [
-                    block[4].strip()
-                    for block in all_blocks
-                    if block[6] == 0
-                ]
+                logging.info("Extracting paragraphs from blocks")
+                paragraphs = self._create_paragraphs(all_blocks)
 
                 logging.info(f"Finish reading {self.file_path}. Extracted {len(paragraphs)} paragraphs")
 
