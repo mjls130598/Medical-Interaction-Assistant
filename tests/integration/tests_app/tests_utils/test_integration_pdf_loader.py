7bfd38cd83e02fd30c33992477636ad6d47bc26c
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2026-03-22 13:06:33 +0100

    test: Integration tests to _clean_block and _create_paragraphs

--- tests/integration/tests_app/tests_utils/test_integration_pdf_loader.py
+++ tests/integration/tests_app/tests_utils/test_integration_pdf_loader.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 
@@ -63,3 +64,156 @@ class TestPdfLoader:
 
             assert len(paragraphs) == 1
             assert "This is a PDF example" in paragraphs
+
+    class TestCleanBlock:
+
+        def test_remove_space_between_sentence(self):
+
+            pdf_path = str(self.base_path / "data" / "separate_sentences.pdf")
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+
+            with patch.object(MedicalPDFLoader, '_clean_block', wraps=pdf_loader._clean_block) as spy_clean:
+                read_paragraph = pdf_loader._read_pdf()[0]
+                real_paragraph = (
+                    "Lea todo el prospecto detenidamente antes de empezar a tomar este medicamento, porque contiene información importante para usted."
+                )
+                
+                assert read_paragraph == real_paragraph
+                assert spy_clean.called
+                assert spy_clean.call_count == 1
+
+        def test_new_lines_between_capital_letters(self):
+
+            pdf_path = str(self.base_path / "data" / "new_lines_capital.pdf")
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+
+            with patch.object(MedicalPDFLoader, '_clean_block', wraps=pdf_loader._clean_block) as spy_clean:
+                read_paragraph = pdf_loader._read_pdf()[0]
+            
+                real_paragraph = (
+                    "Lea todo el prospecto detenidamente antes de empezar a tomar este medicamento, porque contiene información importante para usted.\n"
+                    "Conserve este prospecto, ya que puede tener que volver a leerlo."
+                )
+
+                assert read_paragraph == real_paragraph
+                assert spy_clean.called
+                assert spy_clean.call_count == 1
+
+        def test_new_lines_between_guion(self):
+
+            pdf_path = str(self.base_path / "data" / "new_lines_guion.pdf")
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+
+            with patch.object(MedicalPDFLoader, '_clean_block', wraps=pdf_loader._clean_block) as spy_clean:
+                read_paragraph = pdf_loader._read_pdf()[0]
+            
+                real_paragraph = (
+                    "Lea todo el prospecto detenidamente antes de empezar a tomar este medicamento, porque contiene información importante para usted.\n"
+                    "- Conserve este prospecto, ya que puede tener que volver a leerlo.\n"
+                    "- Si tiene alguna duda, consulte a su médico o farmacéutico.\n"
+                    "- Este medicamento se le ha recetado solamente a usted, y no debe dárselo a otras personas aunque tengan los mismos síntomas que usted, ya que puede perjudicarles.\n"
+                    "- Si experimenta efectos adversos, consulte a su médico o farmacéutico, incluso si se trata de efectos adversos que no aparecen en este prospecto. Ver sección 4."
+                )
+
+                assert read_paragraph == real_paragraph
+                assert spy_clean.called
+                assert spy_clean.call_count == 1
+
+        def test_new_lines_between_number_sections(self):
+
+            pdf_path = str(self.base_path / "data" / "new_lines_number_section.pdf")
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+
+            with patch.object(MedicalPDFLoader, '_clean_block', wraps=pdf_loader._clean_block) as spy_clean:
+                read_paragraph = pdf_loader._read_pdf()[0]
+            
+                real_paragraph = (
+                    "Lea todo el prospecto detenidamente antes de empezar a tomar este medicamento, porque contiene información importante para usted.\n"
+                    "1. Conserve este prospecto, ya que puede tener que volver a leerlo.\n"
+                    "2. Si tiene alguna duda, consulte a su médico o farmacéutico.\n"
+                    "3. Este medicamento se le ha recetado solamente a usted, y no debe dárselo a otras personas aunque tengan los mismos síntomas que usted, ya que puede perjudicarles.\n"
+                    "4. Si experimenta efectos adversos, consulte a su médico o farmacéutico, incluso si se trata de efectos adversos que no aparecen en este prospecto. Ver sección 4."
+                )
+
+                assert read_paragraph == real_paragraph
+                assert spy_clean.called
+                assert spy_clean.call_count == 1
+
+        def test_complete_word(self):
+
+            pdf_path = str(self.base_path / "data" / "complete_word.pdf")
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+
+            with patch.object(MedicalPDFLoader, '_clean_block', wraps=pdf_loader._clean_block) as spy_clean:
+                read_paragraph = pdf_loader._read_pdf()[0]
+            
+                real_paragraph = "Lea todo el prospecto detenidamente"
+
+                assert read_paragraph == real_paragraph
+                assert spy_clean.called
+                assert spy_clean.call_count == 1
+
+    class TestCreateParagraphs:
+
+        def test_remove_page_number(self):
+
+            pdf_path = str(self.base_path / "data" / "number_page.pdf")
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+
+            with patch.object(MedicalPDFLoader, '_create_paragraphs', wraps=pdf_loader._create_paragraphs) as spy_create:
+                read_paragraph = pdf_loader._read_pdf()
+            
+                assert read_paragraph == []
+                assert spy_create.called
+                assert spy_create.call_count == 1
+
+        def test_remove_no_text_block(self):
+
+            pdf_path = str(self.base_path / "data" / "no_text_blocks.pdf")
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+
+            with patch.object(MedicalPDFLoader, '_create_paragraphs', wraps=pdf_loader._create_paragraphs) as spy_create:
+                read_paragraph = pdf_loader._read_pdf()
+            
+                assert read_paragraph == []
+                assert spy_create.called
+                assert spy_create.call_count == 1
+
+        def test_remove_empty_block(self):
+
+            pdf_path = str(self.base_path / "data" / "empty_blocks.pdf")
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+
+            with patch.object(MedicalPDFLoader, '_create_paragraphs', wraps=pdf_loader._create_paragraphs) as spy_create:
+                read_paragraph = pdf_loader._read_pdf()
+            
+                assert read_paragraph == []
+                assert spy_create.called
+                assert spy_create.call_count == 1
+
+        def test_concatenate_incomplete_paragraphs(self):
+            pdf_path = str(self.base_path / "data" / "incomplete_paragraphs.pdf")
+
+            pdf_loader = MedicalPDFLoader(pdf_path)
+
+            with patch.object(MedicalPDFLoader, '_create_paragraphs', wraps=pdf_loader._create_paragraphs) as spy_create:
+                with patch.object(MedicalPDFLoader, '_clean_block', wraps=pdf_loader._clean_block) as spy_clean:
+                    read_paragraph = pdf_loader._read_pdf()
+                    real_paragraphs = [
+                        "Párrafo incompleto continuación del texto.",
+                        "Nuevo párrafo independiente."
+                    ]
+                
+                    assert read_paragraph == real_paragraphs
+                    assert spy_create.called
+                    assert spy_create.call_count == 1
+                    assert spy_clean.called
+                    assert spy_clean.call_count == 3
