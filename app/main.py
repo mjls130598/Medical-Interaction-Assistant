52da19da9fb95a6209cbdd0829f65c7f5b062b62
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2026-05-01 18:27:23 +0200

    refact: Visualice a PDF prospecto extraction

--- app/main.py
+++ app/main.py
@@ -1,18 +1,21 @@
-from rag.pdf_loader import MedicalPDFLoader
-from services.medical_consultant import MedicalConsultant
-from services.vector_db import MedicalVectorDB
-
-
 if __name__ == "__main__":
-    pdf_example = "/home/mjesus/Escritorio/Medical Interaction Assistant/data/input_pdfs/acetilcisteina.pdf" 
+    from rag.loaders.prospecto_loader import ProspectoLoader
+    from rag.cleaners.prospecto_cleaner import ProspectoCleaner
+    from rag.readers.pdf_reader import PDFReader
+    
+    pdf_example = "/home/mjesus/Escritorio/Medical Interaction Assistant/data/input_pdfs/acetilcisteina.pdf"
 
-    pdf_loader = MedicalPDFLoader(pdf_example)
-    documents = pdf_loader.read_load_document()
-    vector_db = MedicalVectorDB()
-    vector_db.add_documents(documents)
+    # Example usage
+    prospecto_cleaner = ProspectoCleaner()
+    pdf_reader = PDFReader()
+    prospecto_loader = ProspectoLoader(reader=pdf_reader, cleaner=prospecto_cleaner, source=pdf_example, cima_id="67763")
 
-    consultant = MedicalConsultant(vector_db)
-    question = "¿Cuáles son las indicaciones de la acetilcisteina?"
-    answer = consultant.ask_question(question)
-    print("Respuesta del asistente médico:")
-    print(answer)
+    documents = prospecto_loader.create_document()
+
+    print(len(documents))
+
+    for i, doc in enumerate(documents[:3]): # Vemos los primeros 3
+        print(f"--- DOCUMENTO {i} ---")
+        print(f"CONTENIDO: {doc.page_content[:100]}...") # Primeros 100 caracteres
+        print(f"METADATOS: {doc.metadata}")
+        print("-" * 30)
